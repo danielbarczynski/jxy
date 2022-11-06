dc1fe73cea7604d09d9fe901add3459b6df3573a
Daniel PC <daniel.barczynski@onet.pl>
2022-11-06 14:29:48 +0100

    json stringify

--- index.js
+++ index.js
@@ -23,7 +23,32 @@ let companies = `[
     }
 ]`;
 
+let companies2 = [
+    {
+        "name": "Corpo",
+        "numberOfEmployes": 10000,
+        "managers": ["Jake", "Mike", "John"],
+        "isTrusted": false
+    },
+
+    {
+        "name": "StartUp",
+        "numberOfEmployes": 5,
+        "managers": null,
+        "isTrusted": true
+    },
+
+    {
+        "name": "Business",
+        "numberOfEmployes": 100,
+        "managers": ["Claudia", "Bob"],
+        "isTrusted": true
+    }
+];
+
 console.log(companies); 
 console.log(JSON.parse(companies));
 
+console.log(companies2);
+console.log(JSON.stringify(companies2));
 
